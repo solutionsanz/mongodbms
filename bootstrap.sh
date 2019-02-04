b741a19239406bce237e541b9facd3efdb15a645
SolutionsANZ <solutionsanz2@gmail.com>
2019-02-04 20:48:55 +1100

    Updating bootstrap with updated version of docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,11 +22,11 @@
     sudo usermod -G docker ubuntu    
     docker --version
 
-    # sudo add-apt-repository \
-    # "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    # $(lsb_release -cs) \
-    # stable"
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
 
-    # sudo apt-get update
+    sudo apt-get update
 
-    # sudo apt-get install docker-ce -y --force-yes
+    sudo apt-get install docker-ce -y --force-yes
